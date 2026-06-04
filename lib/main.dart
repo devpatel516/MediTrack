d03222aba29d6e24cdc65dd0cbd430287e742f8d
unknown <devpatel17207@gmail.com>
2026-06-04 14:04:02 +0530

    App name and Icon changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:internship/screens/login_screen.dart';
 import 'package:provider/provider.dart';
 import 'auth_provider.dart';
-import 'screens/login_screen.dart';
+import 'screens/splash_screen.dart';
 void main() {
   runApp(
     MultiProvider(
@@ -28,33 +29,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body:Center(
-        child: Text('This is test screen'),
-      )
-    );
-  }
-}
